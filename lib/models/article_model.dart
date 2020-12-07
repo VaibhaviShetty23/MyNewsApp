6cf715577b547a7369d48d0dc64eaea8a32718d5
VaibhaviShetty23 <vaibhavishettyme123@gmail.com>
2020-12-07 17:48:17 +0530

    Done

---
+++ lib/models/article_model.dart
@@ -0,0 +1,13 @@
+class ArticleModel
+{
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String content;
+  
+
+  ArticleModel({this.title,this.description,this.author,this.content,
+    this.urlToImage, this.url});
+}
